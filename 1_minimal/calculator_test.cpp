82213c3136d6aa1e17426baca16aed791a8a5454
Satyam <sharma.satyam@outlook.com>
2022-10-22 14:13:39 -0400

    Update calculator_test.cpp

--- 1_minimal/calculator_test.cpp
+++ 1_minimal/calculator_test.cpp
@@ -4,25 +4,25 @@
 TEST(Addition, positive_numbers)
 {
     calculator myCalc;
-    EXPECT_EQ(myCalc.Add(6, 5), 11);
+    EXPECT_EQ(11, myCalc.Add(6, 5));
 }
 
 TEST(Subtraction, positive_numbers)
 {
     calculator myCalc;
-    EXPECT_EQ(myCalc.Subtract(6, 5), 1);
+    EXPECT_EQ(1, myCalc.Subtract(6, 5));
 }
 
 TEST(Multiplication, positive_numbers)
 {
     calculator myCalc;
-    EXPECT_EQ(myCalc.Multiply(5, 6), 30);
+    EXPECT_EQ(30, myCalc.Multiply(5, 6));
 }
 
 TEST(Multiplication, negative_numbers)
 {
     calculator myCalc;
-    EXPECT_EQ(myCalc.Multiply(-5, -6), 30);
+    EXPECT_EQ(30, myCalc.Multiply(-5, -6));
 }
 
 int main(int argc, char **argv)
