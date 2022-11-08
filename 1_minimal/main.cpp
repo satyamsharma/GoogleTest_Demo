573e4640bd9f062953e759cf36a68a18a909d24d
satyamsharma <sharma.satyam@outlook.com>
2022-11-08 10:56:23 -0500

    fix typo

--- 1_minimal/main.cpp
+++ 1_minimal/main.cpp
@@ -7,8 +7,8 @@ int main()
 {
     calculator myCalc;
     cout << "Addition 5 + 6 = " << myCalc.Add(5, 6) << endl;
-    cout << "Subtract 5 + 6 = " << myCalc.Subtract(5, 6) << endl;
-    cout << "Multiply 5 + 6 = " << myCalc.Multiply(5, 6) << endl;
+    cout << "Subtract 5 - 6 = " << myCalc.Subtract(5, 6) << endl;
+    cout << "Multiply 5 * 6 = " << myCalc.Multiply(5, 6) << endl;
 
     return 0;
 }
